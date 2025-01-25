15a5a97bfaa7d53a4a30118c11233d88195bfc55
RamKimura <yuto.kimura@sansan.com>
2025-01-25 14:32:06 +0900

    add ktor routing

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +9,12 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/test1") {
+            val text = "<h1>Hello World!</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
