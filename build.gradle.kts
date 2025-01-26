7bf11f5b2d29c18e52fe0502dacec67876c77ead
RamKimura <yuto.kimura@sansan.com>
2025-01-26 19:26:50 +0900

    install ktor status pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,10 +25,10 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.ktor.server.status.pages)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.assertj.core)
-
 }
 
 tasks.test {
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -2,10 +2,17 @@ package com.example
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(StatusPages) {
+        exception<IllegalArgumentException> { call, cause ->
+            call.respondText("App in illegal state as ${cause.message}")
+        }
+    }
+
     routing {
         get("/") {
             call.respondText("Hello World!")
@@ -15,6 +22,10 @@ fun Application.configureRouting() {
             val text = "<h1>Hello World!</h1>"
             val type = ContentType.parse("text/html")
             call.respondText(text, type)
+        }
+
+        get("/error-test") {
+            throw IllegalArgumentException("Too Busy")
         }
     }
 }
