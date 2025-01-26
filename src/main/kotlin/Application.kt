290efe004b09949ad09c372a28b22ef88ac6d2e3
RamKimura <yuto.kimura@sansan.com>
2025-01-27 06:33:23 +0900

    enable ktlint

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,7 +3,8 @@ package com.example
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    io.ktor.server.netty.EngineMain
+        .main(args)
 }
 
 fun Application.module() {
