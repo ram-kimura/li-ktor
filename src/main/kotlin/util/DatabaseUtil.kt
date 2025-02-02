ff2c3ab4bf511ecf6e74a6d215601e1b2c93d3dc
RamKimura <yuto.kimura@sansan.com>
2025-02-02 17:14:41 +0900

    add: HikariConfig, HikariDataSource

--- src/main/kotlin/util/DatabaseUtil.kt
+++ src/main/kotlin/util/DatabaseUtil.kt
@@ -1,4 +1,17 @@
 package com.example.util
 
-class DatabaseUtil {
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+val config by lazy {
+    HikariConfig().apply {
+        jdbcUrl = "jdbc:postgresql://localhost:5432/li?currentSchema=li"
+        username = "postgres"
+        password = "example"
+        maximumPoolSize = 3
+    }
+}
+
+val dataSource by lazy {
+    HikariDataSource(config)
 }
