d7c91677ec6d810e79fb3578a1de71b3300ee6fa
RamKimura <yuto.kimura@sansan.com>
2025-02-04 15:08:27 +0900

    add: JDBI logger

--- src/main/kotlin/util/DatabaseUtil.kt
+++ src/main/kotlin/util/DatabaseUtil.kt
@@ -3,6 +3,7 @@ package com.example.util
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.statement.Slf4JSqlLogger
 
 val config by lazy {
     HikariConfig().apply {
@@ -22,5 +23,6 @@ val dataSource by lazy {
 val jdbi by lazy {
     Jdbi.create(dataSource).apply {
         installPlugins()
+        setSqlLogger(Slf4JSqlLogger())
     }
 }
